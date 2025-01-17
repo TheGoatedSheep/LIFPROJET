b29ba568a0915be4037aca448ba49c407a1dd795
allister <allister@Linuxbuntu.myguest.virtualbox.org>
2025-01-17 21:05:38 +0100

    Ajout du dossier Frontend/

---
+++ src/Frontend/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+function index() {
+    return (
+        <div className={styles.indexDiv}>
+            
+        </div>
+    )
+}
+
+export default index;
