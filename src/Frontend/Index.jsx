37bb7e2568694fb06ba530b42f87797fa58db493
MeAllister <anselmeyeffe@gmail.com>
2025-01-17 22:01:12 +0100

    Test icon

--- src/Frontend/Index.jsx
+++ src/Frontend/Index.jsx
@@ -1,11 +1,11 @@
 import styles from './index.module.css'
 
-function index() {
+function Index() {
     return (
         <div className={styles.indexDiv}>
-
+            <i className={`${styles.icon} fa-solid fa-user-secret`} />
         </div>
     )
 }
 
-export default index;
+export default Index;
